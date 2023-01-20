f2cb11b351a63beed94f4d811c1ebe422c38255c
Daniil <laito.treasure@gmail.com>
2023-01-20 21:39:05 +0300

    create lightbox js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImg = document.querySelector('.gallery');
+const galleryCards = createGallery(galleryItems);
+
+galleryImg.innerHTML = galleryCards;
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => 
+    
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    /></a>
+    </div>`).join('');
+};
+
+galleryImg.addEventListener('click', onSmallImageClick);
+
+function onSmallImageClick(event) {
+    event.preventDefault();
+    const isImage = event.target.classList.contains('gallery__image');
+    if (!isImage) {
+        return;
+    }
+
+    const urlOfBigImage = event.target.dataset.source;
+    
+    openModalBigImage(urlOfBigImage);
+}
+
+function openModalBigImage(url) {
+    const closeModal = event => {
+        if (event.code === 'Escape') { 
+            instance.close(); 
+        }
+    };
+
+    const instance = basicLightbox.create(`
+		<img src="${url}">`,
+        {
+            onShow: () =>
+                document.addEventListener('keydown', closeModal),
+            onClose: () =>
+                document.removeEventListener('keydown', closeModal)
+        });
+    instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImg = document.querySelector('.gallery');
+const galleryCards = createGallery(galleryItems);
+
+
+galleryImg.innerHTML = galleryCards;
+
+function createGallery(items) {
+    return items.map(({ preview, original, description }) => 
+    
+        `<a class="gallery__item" href="${original}">
+         <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+         />
+        </a>
+        `
+    ).join('');
+};
+
+function imageClick(event) {
+    event.preventDefault();
+    
+    const isImage = event.target.classList.contains('gallery__image');
+    if (!isImage) {
+        return;
+    }
+
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionSelector:'img',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
+ 
